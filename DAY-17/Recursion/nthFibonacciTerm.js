d252454679e65086d4962eb95763beec83fe9ba0
Punit Sahu <therealpunit5@gmail.com>
2026-01-12 12:41:03 +0530

    Recursion: solved core problems and edge cases

--- DAY-17/Recursion/nthFibonacciTerm.js
+++ DAY-17/Recursion/nthFibonacciTerm.js
@@ -1,10 +1,4 @@
 // ? Q. Find nth fibonacci term using recursion.
-function nthFibonacci(n){
-    if(n === 0 || n === 1) return n;
-    return nthFibonacci(n - 1) + nthFibonacci(n - 2);
-}
-
-// * Method 2 -
 function nthFibonacci2(n){
     if(n <= 0) return null;
     function helper(pre, cur, i){
@@ -14,16 +8,8 @@ function nthFibonacci2(n){
     return helper(0, 1, 1);
 }
 
-function isSorted20(arr){
-    function helper(i){
-        if(i === arr.length - 1) return true;
-        if(arr[i - 1] > arr[i]) return false;
-        return helper(i + 1);
-    }
-    return helper(1);
-}
-
-function isSorted30(arr, n){
-    if(n === 0 || n === 1) return true;
-    return (arr[n - 1] >= arr[n - 2]) && (isSorted30(arr, n - 1))
+// * Method 2 -
+function nthFibonacci(n){
+    if(n === 0 || n === 1) return n;
+    return nthFibonacci(n - 1) + nthFibonacci(n - 2);
 }
